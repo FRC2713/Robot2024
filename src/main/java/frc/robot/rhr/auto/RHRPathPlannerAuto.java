1baa3aca6fa132fb4daef3c82e6a4d2c12511180
Justin <justin@tervay.com>
2024-01-16 18:09:14 -0500

    add error tracker

---
+++ src/main/java/frc/robot/rhr/auto/RHRPathPlannerAuto.java
@@ -0,0 +1,53 @@
+package frc.robot.rhr.auto;
+
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.util.PathPlannerLogging;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.Robot;
+import frc.robot.util.ErrorTracker;
+import org.littletonrobotics.junction.Logger;
+
+public class RHRPathPlannerAuto extends PathPlannerAuto {
+
+  ErrorTracker errorTracker;
+
+  public RHRPathPlannerAuto(String autoName) {
+    this(autoName, 10);
+  }
+
+  public RHRPathPlannerAuto(String autoName, int errorTrackerSamples) {
+    super(autoName);
+    errorTracker = new ErrorTracker(errorTrackerSamples);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    super.end(interrupted);
+    errorTracker.printSummary(this.getName());
+  }
+
+  @Override
+  public void initialize() {
+    super.initialize();
+    errorTracker.reset();
+
+    PathPlannerLogging.setLogTargetPoseCallback(
+        (targetPose) -> {
+          Pose2d currentPose = Robot.swerveDrive.getUsablePose();
+          var error =
+              new Pose2d(
+                  new Translation2d(
+                      targetPose.getX() - currentPose.getX(),
+                      targetPose.getY() - currentPose.getY()),
+                  Rotation2d.fromRadians(
+                      targetPose.getRotation().getRadians()
+                          - currentPose.getRotation().getRadians()));
+
+          errorTracker.addObservation(error);
+          Logger.recordOutput("Pathplanner/Target Pose", targetPose);
+          Logger.recordOutput("Pathplanner/Pose Error", error);
+        });
+  }
+}
