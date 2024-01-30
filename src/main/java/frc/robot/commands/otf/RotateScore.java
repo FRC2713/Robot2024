ba9c896ea4b9f432dc3cfbaa92c3f2095c20847d
stem-bud <106452553+stem-bud@users.noreply.github.com>
2024-01-30 10:15:05 -0500

    optimal speaker location

--- src/main/java/frc/robot/commands/otf/RotateScore.java
+++ src/main/java/frc/robot/commands/otf/RotateScore.java
@@ -7,15 +7,28 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Robot;
+import frc.robot.util.FieldConstants;
+import frc.robot.util.RedHawkUtil;
 import frc.robot.util.SwerveHeadingController;
 import org.littletonrobotics.junction.Logger;
 
 public class RotateScore extends SequentialCommandGroup {
   public static double getOptimalAngle(Pose2d position) {
-    var speakerLoc = new Translation2d(0.695, 5.552);
+    var supposedSpeakerLoc = new Translation2d(0.695, 5.552);
+    var speakerLoc = supposedSpeakerLoc;
+    if (!RedHawkUtil.pastMidPoint(position)) {
+      speakerLoc =
+          new Translation2d(
+              FieldConstants.fieldLength - supposedSpeakerLoc.getX(), supposedSpeakerLoc.getY());
+    }
+
     var distance = position.getTranslation().getDistance(speakerLoc);
+    /*var optimalAngle =
+    Math.atan2((speakerLoc.getY() - position.getY()), (speakerLoc.getX() - position.getX()));*/
     var optimalAngle = Math.acos((position.getX() - speakerLoc.getX()) / distance);
-    if (position.getY() < speakerLoc.getY()) optimalAngle *= -1;
+    if (position.getY() < speakerLoc.getY()) {
+      optimalAngle *= -1;
+    }
     Logger.recordOutput(
         "Optimal Angle", new Pose2d(position.getTranslation(), new Rotation2d(optimalAngle)));
     return optimalAngle;
