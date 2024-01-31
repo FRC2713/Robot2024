8dec09877ee4c9049b9828d860c08ec81110bcb3
stem-bud <106452553+stem-bud@users.noreply.github.com>
2024-01-31 18:22:07 -0500

    optimal speaker 2

--- src/main/java/frc/robot/commands/otf/RotateScore.java
+++ src/main/java/frc/robot/commands/otf/RotateScore.java
@@ -7,20 +7,13 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Robot;
-import frc.robot.util.FieldConstants;
 import frc.robot.util.RedHawkUtil;
 import frc.robot.util.SwerveHeadingController;
 import org.littletonrobotics.junction.Logger;
 
 public class RotateScore extends SequentialCommandGroup {
-  public static double getOptimalAngle(Pose2d position) {
-    var supposedSpeakerLoc = new Translation2d(0.695, 5.552);
-    var speakerLoc = supposedSpeakerLoc;
-    if (!RedHawkUtil.pastMidPoint(position)) {
-      speakerLoc =
-          new Translation2d(
-              FieldConstants.fieldLength - supposedSpeakerLoc.getX(), supposedSpeakerLoc.getY());
-    }
+  public static Rotation2d getOptimalAngle(Pose2d position) {
+    var speakerLoc = RedHawkUtil.Reflections.reflectIfRed(new Translation2d(0.695, 5.552));
     var distance = position.getTranslation().getDistance(speakerLoc);
 
     var optimalAngle = Math.acos((position.getX() - speakerLoc.getX()) / distance);
@@ -29,14 +22,14 @@ public class RotateScore extends SequentialCommandGroup {
     }
     Logger.recordOutput(
         "Optimal Angle", new Pose2d(position.getTranslation(), new Rotation2d(optimalAngle)));
-    return optimalAngle;
+    return RedHawkUtil.Reflections.reflectIfRed(new Rotation2d(optimalAngle));
   }
 
   public static Command goOptimalAngle() {
     return new InstantCommand(
         () -> {
-          double optimal = RotateScore.getOptimalAngle(Robot.swerveDrive.getUsablePose());
-          SwerveHeadingController.getInstance().setSetpoint(new Rotation2d(optimal));
+          Rotation2d optimal = RotateScore.getOptimalAngle(Robot.swerveDrive.getUsablePose());
+          SwerveHeadingController.getInstance().setSetpoint(optimal);
         });
   }
 }
