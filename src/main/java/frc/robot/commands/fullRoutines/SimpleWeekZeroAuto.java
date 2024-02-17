53cd61b91a7031d68a9767c40fde939e971d85d2
2713driverstation <frc2713@gmail.com>
2024-02-17 10:37:19 -0500

    Automated commit at 02/17/2024 10:37:19

--- src/main/java/frc/robot/commands/fullRoutines/SimpleWeekZeroAuto.java
+++ src/main/java/frc/robot/commands/fullRoutines/SimpleWeekZeroAuto.java
@@ -12,12 +12,13 @@ public class SimpleWeekZeroAuto extends SequentialCommandGroup {
   public SimpleWeekZeroAuto() {
     addCommands(
         Shooter.Commands.setMotionMode(Shooter.MotionMode.FENDER_SHOT_CLOSED_LOOP),
-        new WaitUntilCommand(Robot.shooter::isAtTarget),
+        // new WaitUntilCommand(Robot.shooter::isAtTarget),
         Feeder.Commands.setMotionMode(Feeder.MotionMode.SEND_TO_SHOOTER),
         Intake.Commands.setMotionMode(Intake.MotionMode.SEND_GP_TO_FEEDER),
         new WaitCommand(3),
         Feeder.Commands.setMotionMode(Feeder.MotionMode.OFF),
         Shooter.Commands.setMotionMode(Shooter.MotionMode.OFF),
-        Intake.Commands.setMotionMode(Intake.MotionMode.OFF));
+        Intake.Commands.setMotionMode(Intake.MotionMode.OFF)
+        );
   }
 }
