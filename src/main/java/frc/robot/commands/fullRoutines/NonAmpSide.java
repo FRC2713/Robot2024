24c932f48d71136a612d5d30d7427b94e311635d
Sidney Trzepacz <74916637+Sid220@users.noreply.github.com>
2024-02-29 19:24:07 -0500

    preload

--- src/main/java/frc/robot/commands/fullRoutines/NonAmpSide.java
+++ src/main/java/frc/robot/commands/fullRoutines/NonAmpSide.java
@@ -2,6 +2,8 @@ package frc.robot.commands.fullRoutines;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.Robot;
 import frc.robot.commands.ShootingCommands;
 import frc.robot.subsystems.shooterIO.Shooter;
 import frc.robot.subsystems.shooterPivot.ShooterPivot;
@@ -12,29 +14,25 @@ public class NonAmpSide extends SequentialCommandGroup {
   public NonAmpSide() {
     addCommands(
         SwerveSubsystem.Commands.resetOdometry("Non Amp Side.1"),
+
+        // Preload
+        ShootingCommands.runShooterPivot(ShooterPivot.State.FENDER_SHOT),
+        new WaitUntilCommand(Robot.shooterPivot::isAtTargetAngle),
+        ShootingCommands.runShooter(Shooter.State.FENDER_SHOT),
+
+        // First Piece
         ShootingCommands.runPathAndIntake("Non Amp Side.1"),
         ShootingCommands.runPathAndShoot(
             "Non Amp Side.2",
             Shooter.State.AUTO_SHOT_NonAmpSide_1,
             ShooterPivot.State.AUTO_SHOT_NonAmpSide_1),
+
+        // Second Piece
         ShootingCommands.runPathAndIntake("Non Amp Side.3"),
         ShootingCommands.runPathAndShoot(
             "Non Amp Side.4",
             Shooter.State.AUTO_SHOT_NonAmpSide_2,
             ShooterPivot.State.AUTO_SHOT_NonAmpSide_2),
-
-        // new ParallelCommandGroup(
-        //     SwerveSubsystem.Commands.choreoCommandBuilder(firstTraj),
-        //     Intake.Commands.setMotionMode(Intake.State.INTAKE_GP)),
-        // ShootingCommands.FullShotCommands(),
-        // new ParallelCommandGroup(
-        //     SwerveSubsystem.Commands.choreoCommandBuilder(secondTraj),
-        //     Intake.Commands.setMotionMode(Intake.State.INTAKE_GP)),
-        // ShootingCommands.FullShotCommands(),
-        // new ParallelCommandGroup(
-        //     SwerveSubsystem.Commands.choreoCommandBuilder(thirdTraj),
-        //     Intake.Commands.setMotionMode(Intake.State.INTAKE_GP)),
-        // ShootingCommands.FullShotCommands(),
         new InstantCommand(
             () -> {
               SwerveSubsystem.Commands.errorTracker.printSummary("NonAmpSide");
