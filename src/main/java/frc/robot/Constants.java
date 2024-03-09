bc72919650adbc4e882ad414b339a0a684596612
2713driverstation <frc2713@gmail.com>
2024-03-09 15:48:12 -0500

    Automated deploy commit at 03/09/2024 15:48:12

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -246,7 +246,13 @@ public final class Constants {
 
     public static final double HEADING_CONTROLLER_DRIVER_CHANGE_RATE = 4;
     public static final PIDFFGains K_HEADING_CONTROLLER_GAINS =
-        PIDFFGains.builder().name("Heading Controller").kP(8).kD(0.25).kS(2).build().buildTunables();
+        PIDFFGains.builder()
+            .name("Heading Controller")
+            .kP(8)
+            .kD(0)
+            .kS(2)
+            .build()
+            .buildTunables();
 
     public static final ModuleInfo FRONT_LEFT =
         ModuleInfo.builder()
