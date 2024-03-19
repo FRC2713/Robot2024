e24734f2534cce3800d6465c6e2d3773f6b9f713
Sidney Trzepacz <74916637+Sid220@users.noreply.github.com>
2024-03-19 16:41:54 -0400

    formatting

--- src/main/java/frc/robot/VehicleState.java
+++ src/main/java/frc/robot/VehicleState.java
@@ -140,8 +140,12 @@ public class VehicleState {
     SwerveHeadingController.getInstance().setSetpoint(Rotation2d.fromDegrees(angle).plus(GPyaw));
 
     double xSpeed =
-        Math.abs(MathUtil.applyDeadband(-Robot.driver.getLeftY(), DriveConstants.K_JOYSTICK_TURN_DEADZONE))
-        + Math.abs(MathUtil.applyDeadband(-Robot.driver.getLeftX(), DriveConstants.K_JOYSTICK_TURN_DEADZONE));
+        Math.abs(
+                MathUtil.applyDeadband(
+                    -Robot.driver.getLeftY(), DriveConstants.K_JOYSTICK_TURN_DEADZONE))
+            + Math.abs(
+                MathUtil.applyDeadband(
+                    -Robot.driver.getLeftX(), DriveConstants.K_JOYSTICK_TURN_DEADZONE));
 
     return ChassisSpeeds.fromRobotRelativeSpeeds(
         xSpeed,
