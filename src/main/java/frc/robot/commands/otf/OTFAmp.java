6d2378bd21767483f6646488f58dc331eb52b501
Sidney Trzepacz <74916637+Sid220@users.noreply.github.com>
2024-03-27 18:58:16 -0400

    otf amp

--- src/main/java/frc/robot/commands/otf/OTFAmp.java
+++ src/main/java/frc/robot/commands/otf/OTFAmp.java
@@ -9,7 +9,6 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
