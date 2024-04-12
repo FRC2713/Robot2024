ff35f069c9cb426c686ef04db9e3a8be678ce5fc
Sidney Trzepacz <74916637+Sid220@users.noreply.github.com>
2024-04-12 14:35:23 -0400

    Four Piece centre align to tag

--- src/main/java/frc/robot/commands/fullRoutines/FourPieceCentre.java
+++ src/main/java/frc/robot/commands/fullRoutines/FourPieceCentre.java
@@ -3,14 +3,18 @@ package frc.robot.commands.fullRoutines;
 import com.choreo.lib.Choreo;
 import com.choreo.lib.ChoreoTrajectory;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.commands.Cmds;
 import frc.robot.commands.RHRFullRoutine;
 import frc.robot.commands.ShootingCommands;
 import frc.robot.subsystems.intakeIO.Intake;
 import frc.robot.subsystems.shooterIO.Shooter;
+import frc.robot.subsystems.shooterIO.Shooter.ShooterState;
 import frc.robot.subsystems.shooterPivot.ShooterPivot;
 import frc.robot.subsystems.swerveIO.SwerveSubsystem;
+import frc.robot.subsystems.swerveIO.SwerveSubsystem.MotionMode;
 import frc.robot.util.RedHawkUtil;
+import frc.robot.util.SwerveHeadingController;
 
 public class FourPieceCentre extends RHRFullRoutine {
 
@@ -31,29 +35,42 @@ public class FourPieceCentre extends RHRFullRoutine {
         ShootingCommands.runShooterPivot(ShooterPivot.State.FENDER_SHOT),
         ShootingCommands.runShooter(Shooter.ShooterState.NO_DIFFERENTIAL_SHOT),
         RedHawkUtil.logShot(),
+        Cmds.setState(ShooterState.DIFFERENTIAL_SHOT),
 
         // First Piece
         ShootingCommands.runPathAndIntake(traj1),
         new WaitCommand(0.7),
+        Cmds.setState(MotionMode.ALIGN_TO_TAG),
+        Cmds.setState(ShooterPivot.State.POSE_AIM),
+        new WaitUntilCommand(() -> SwerveHeadingController.getInstance().atSetpoint(0.3)),
         ShootingCommands.runShooterAndPivot(
             Shooter.ShooterState.DIFFERENTIAL_SHOT, ShooterPivot.State.POSE_AIM),
         RedHawkUtil.logShot(),
+        Cmds.setState(MotionMode.TRAJECTORY),
         Cmds.setState(ShooterPivot.State.INTAKING),
 
         // Second Piece
         ShootingCommands.runPathAndIntake(traj2),
         new WaitCommand(0.7),
+        Cmds.setState(MotionMode.ALIGN_TO_TAG),
+        Cmds.setState(ShooterPivot.State.POSE_AIM),
+        new WaitUntilCommand(() -> SwerveHeadingController.getInstance().atSetpoint(0.3)),
         ShootingCommands.runShooterAndPivot(
             Shooter.ShooterState.DIFFERENTIAL_SHOT, ShooterPivot.State.POSE_AIM),
         RedHawkUtil.logShot(),
+        Cmds.setState(MotionMode.TRAJECTORY),
         Cmds.setState(ShooterPivot.State.INTAKING),
 
         // Third Piece
         ShootingCommands.runPathAndIntake(traj3),
         new WaitCommand(0.7),
+        Cmds.setState(MotionMode.ALIGN_TO_TAG),
+        Cmds.setState(ShooterPivot.State.POSE_AIM),
+        new WaitUntilCommand(() -> SwerveHeadingController.getInstance().atSetpoint(0.3)),
         ShootingCommands.runShooterAndPivot(
             Shooter.ShooterState.DIFFERENTIAL_SHOT, ShooterPivot.State.POSE_AIM),
         RedHawkUtil.logShot(),
+        Cmds.setState(MotionMode.TRAJECTORY),
         Cmds.setState(ShooterPivot.State.INTAKING),
 
         // Reset everything for teleop
