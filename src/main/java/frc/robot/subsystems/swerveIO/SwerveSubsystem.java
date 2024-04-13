b403af75e1e6cb855ed897fefd4fac566a796551
2713driverstation <frc2713@gmail.com>
2024-04-13 14:33:19 -0400

    Automated deploy commit at 04/13/2024 14:33:19

--- src/main/java/frc/robot/subsystems/swerveIO/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swerveIO/SwerveSubsystem.java
@@ -301,7 +301,13 @@ public class SwerveSubsystem extends SubsystemBase {
   public void updatePoseEstimatorWithVisionBotPoseMegaTag2(
       VisionInfo visionInfo, VisionInputs visionInputs) {
     LimelightHelpers.SetRobotOrientation(
-        visionInfo.getNtTableName(), Rotation2d.fromDegrees(inputs.gyroYawPosition).rotateBy(Rotation2d.fromDegrees(180)), inputs.gyroYawVelocity, 0, 0, 0, 0);
+        visionInfo.getNtTableName(),
+        Rotation2d.fromDegrees(inputs.gyroYawPosition).unaryMinus().getDegrees(),
+        inputs.gyroYawVelocity,
+        0,
+        0,
+        0,
+        0);
 
     var doRejectUpdate = false;
     double xyStds = .6;
